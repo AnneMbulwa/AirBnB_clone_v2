4f318de80e23cd8a85318af3f60e1ac397eb3266
AnneMbulwa <annembulwa602@gmail.com>
2024-01-16 21:13:57 +0300

    airbnb clone v2

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Prepares a MySQL server for the project
+-- Create a hbnb_test_db database with password hbnb_test  in localhost
+-- Grant all privileges on hbnb_test_db
+-- Grant select privileges on performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
