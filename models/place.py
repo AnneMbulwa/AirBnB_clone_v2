73637b5a0c2594e9ad741852464bdbe28b48a0a3
AnneMbulwa <annembulwa602@gmail.com>
2024-01-17 22:08:22 +0300

    airbnb clone v2

--- models/place.py
+++ models/place.py
@@ -14,6 +14,14 @@ from sqlalchemy import String
 from sqlalchemy import Table
 from sqlalchemy.orm import relationship
 
+relation_table = Table("place_amenity", Base.metadata,
+                       Column("place_id", String(60), ForeignKey("places.id"),
+                              primary_key=True, nullable=False),
+                       Column("amenity_id", String(60),
+                              ForeignKey("amenities.id"), primary_key=True,
+                              nullable=False)
+                       )
+
 
 class Place(BaseModel, Base):
     """
@@ -35,3 +43,32 @@ class Place(BaseModel, Base):
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
                              viewonly=False)
+
+    amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """list of all linked Reviews."""
+            mod = models.storage.all(Review)
+            x_review = []
+            for review in (mod.values()):
+                if review.place_id == self.id:
+                    x_review.append(review)
+            return x_review
+
+        @property
+        def amenities(self):
+            """list set of linked amenities"""
+            mod = models.storage.all(Amenities)
+            x_amenity = []
+            for amenity in list(mod.values()):
+                if amenity.id in self.amenity_ids:
+                    x_amenity.append(amenity)
+            return x_amenity
+
+        @amenity.setter
+
+        def amenities(self, value):
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
