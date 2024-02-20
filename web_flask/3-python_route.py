f136cd36f9ab627419a3b20715d9dc440fb9fd22
AnneMbulwa <annembulwa602@gmail.com>
2024-02-20 16:29:39 +0300

    flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -12,10 +12,12 @@ def hello_hbnb():
     """display hello hbnb"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display hbnb"""
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
@@ -24,6 +26,7 @@ def c(text):
     """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
